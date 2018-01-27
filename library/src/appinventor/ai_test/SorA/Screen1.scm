ac023928a9864455c794c22be515d4255085b4d4
guyutongxue <guyutongxue@163.com>
2018-01-27 22:46:29 +0800

    update to RC_0.6.1.2

--- library/src/appinventor/ai_test/SorA/Screen1.scm
+++ library/src/appinventor/ai_test/SorA/Screen1.scm
@@ -1,4 +1,4 @@
 #|
 $JSON
-{"authURL":["localhost"],"YaVersion":"159","Source":"Form","Properties":{"$Name":"Screen1","$Type":"Form","$Version":"20","AboutScreen":"SorA (SorA: A Homework Tracking App) by Guyutongxue","AppName":"SorA","CloseScreenAnimation":"fade","Icon":"AppIcon~1000x1000.png","OpenScreenAnimation":"fade","ScreenOrientation":"portrait","ShowListsAsJson":"True","Title":"Screen1","TitleVisible":"False","Uuid":"0","VersionCode":"20180125","VersionName":"RC_0.6.1.1","$Components":[{"$Name":"DB","$Type":"TinyDB","$Version":"1","Uuid":"539694209"}]}}
+{"authURL":["localhost"],"YaVersion":"159","Source":"Form","Properties":{"$Name":"Screen1","$Type":"Form","$Version":"20","AboutScreen":"SorA (SorA: A Homework Tracking App) by Guyutongxue","AppName":"SorA","CloseScreenAnimation":"fade","Icon":"AppIcon~1000x1000.png","OpenScreenAnimation":"fade","ScreenOrientation":"portrait","ShowListsAsJson":"True","Title":"Screen1","TitleVisible":"False","Uuid":"0","VersionCode":"20180127","VersionName":"RC_0.6.1.2","$Components":[{"$Name":"DB","$Type":"TinyDB","$Version":"1","Uuid":"539694209"}]}}
 |#
